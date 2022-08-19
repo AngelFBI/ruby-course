2579619075338a86cb73ac9977390636d6607c81
AngelFBI <angel.bejarano@enroutesystems.com>
2022-08-19 11:43:37 -0500

    Exercise to learn more about symbols

---
+++ exercise_ruby/x05HashSymbol.rb
@@ -0,0 +1,13 @@
+# En un hash el symbol cuenta para el all_symbols.size
+
+# Hipotesis: Si se guardara en el all_symbols.size
+
+person = {name: "Paco"}
+animal = {name: "Firulais"}
+puts Symbol.all_symbols.size
+
+
+# Conclusion
+
+# Paco
+# Los symbols solo se almacenan una vez en el espacio de memoria
